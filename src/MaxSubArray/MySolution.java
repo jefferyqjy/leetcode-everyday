773ab4b7c8480a0a07f9f19e92ae9592867c3f1b
jefferyqjy <435542925@qq.com>
2021-10-04 15:58:05 +0800

    + maxSubArray

---
+++ src/MaxSubArray/MySolution.java
@@ -0,0 +1,25 @@
+package MaxSubArray;
+
+/**
+ * @description: https://leetcode-cn.com/problems/maximum-subarray/
+ * @author: jefferyqjy
+ * @datetime: 2021/9/27 10:26
+ */
+public class MySolution {
+
+    /**
+     * 没写出来
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int result = 0;
+
+        return result;
+    }
+
+}

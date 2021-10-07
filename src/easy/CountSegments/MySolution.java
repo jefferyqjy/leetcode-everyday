0bb27bd8cab76b4272ebc4476e61191bde3b0000
jefferyqjy <435542925@qq.com>
2021-10-07 16:00:51 +0800

    + CountSegments

---
+++ src/easy/CountSegments/MySolution.java
@@ -0,0 +1,30 @@
+package easy.CountSegments;
+
+/**
+ * @description:
+ * @author: jefferyqjy
+ * @datetime: 2021/10/7 15:49
+ */
+public class MySolution {
+
+    public static void main(String[] args) {
+        String s = ", , , ,        a, eaefa";
+        countSegments(s);
+    }
+
+    public static int countSegments(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        String[] strArray = s.split(" ");
+        int length = 0;
+        for (int i = 0; i < strArray.length; i++) {
+            String str = strArray[i];
+            if (!str.isEmpty()) {
+                length++;
+            }
+        }
+        return length;
+    }
+}
